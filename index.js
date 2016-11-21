1242ef57f6d76b42017f4773406e405a5284353b
benjamin wilson <benjamin@asmusas.net>
2016-11-21 15:38:39 +0100

    related arxiv articles and related talks

--- index.js
+++
@@ -1,3 +0,0 @@
-$().ready(function() {
-	$('.add-to-schedule').hide();
-});
--- nips_api.py
+++ nips_api.py
@@ -5,6 +5,7 @@ import tornado.web
 import tornado.template
 import logging
 import ujson
+import requests
 
 from requests import HTTPError
 from lateral.api import Api
@@ -16,6 +17,10 @@ COOKIE_NAME = 'nipsscheduler'
 CONTENT_TYPE = "Content-Type"
 APP_JSON = "application/json"
 
+NUM_RESULTS = 4
+
+ARXIV_ENDPOINT = 'http://arxiv-api.lateral.io'
+
 #FIXME be consistent: documents or events?
 
 
@@ -24,28 +29,43 @@ def document_to_card(document):
     meta = document['meta']
     authors = meta['authors'].split(', ')
     author_ids = meta['author_ids'].split(', ')
-    return dict(id=document['id'], meta=meta, authors=authors, author_ids=author_ids)
+    result = dict(id=document['id'], meta=meta, authors=authors,
+                  author_ids=author_ids)
+    if 'similarity' in document:
+        result['similarity'] = document['similarity']
+    return result
+
 
 class APIHandler(tornado.web.RequestHandler):
 
     def initialize(self, api):
         self.api = api
 
-    def get_card(self, event_id):
-        response = self.api.get_document(event_id)
-        document = ujson.loads(response.text)
+    def get_event(self, event_id):
+        document = self.api.get_document(event_id)
         return document_to_card(document)
 
     def get_schedule_cards(self, user_id):
         # get preferences for the user
-        request = self.api.get_users_preferences(user_id)
-        prefs = ujson.loads(request.text)
+        prefs = self.api.get_users_preferences(user_id)
         event_ids = [pref['document_id'] for pref in prefs]
         # get all the documents, one by one
-        cards = [self.get_card(event_id) for event_id in event_ids]
+        cards = [self.get_event(event_id) for event_id in event_ids]
         # TODO sort by start_time_numeric
         return cards
 
+    def get_related_papers(self, text):
+        """
+        Return the related arXiv papers according to the Lateral API as a
+        Python list in the formated described here
+        https://lateral.io/docs/arxiv-recommender/reference#arxiv-recommend-by-text-post
+        """
+        url = ARXIV_ENDPOINT + '/recommend-by-text/'
+        payload = ujson.dumps(dict(text=text))
+        headers = { 'subscription-key': self.api.key, CONTENT_TYPE: APP_JSON }
+        response = requests.request("POST", url, data=payload, headers=headers)
+        results = ujson.loads(response.text)
+        return results[:NUM_RESULTS]
 
 class UserHandler(APIHandler):
 
@@ -53,23 +73,21 @@ class UserHandler(APIHandler):
         self.user_id = self.get_cookie(COOKIE_NAME)
         if not self.user_id:  # never seen user before
             # create a new user in the API
-            response = self.api.post_user()
-            user = ujson.loads(response.text)
+            user = self.api.post_user()
             self.user_id = user['id']
             # set the cookie to be their user id
             self.set_cookie(COOKIE_NAME, self.user_id)  # FIXME check expiry in browser
 
 
-class RootHandler(UserHandler):
+class EventsHandler(UserHandler):
 
     def get(self):
         # get all the events
-        request = self.api.get_documents()
-        documents = ujson.loads(request.text)
+        documents = self.api.get_documents()
         event_cards = [document_to_card(document) for document in documents]
         # get the user's schedule
         schedule_cards = self.get_schedule_cards(self.user_id)
-        self.render('index.html',
+        self.render('events.html',
                     schedule_cards=schedule_cards,
                     event_cards=event_cards,
                     user_id=self.user_id)
@@ -78,17 +96,23 @@ class RootHandler(UserHandler):
 class EventHandler(UserHandler):
 
     def get(self, event_id):
-        #TODO fetch the similar talks, include them
-        #TODO fetch the similar arxiv papers, include them
-        card = self.get_card(event_id)
-        self.render('event.html', related_cards=[], **card)
+        event = self.get_event(event_id)
+        # FIXME fetch the tags
+
+        # fetch the similar talks
+        related_events = self.api.get_documents_similar(
+            event_id, fields='meta,text', exclude='[%s]' % event_id,
+            number=NUM_RESULTS)
+        related_events = map(document_to_card, related_events)
+        # fetch the similar arxiv papers, include them
+        related_papers = self.get_related_papers(event['meta']['abstract_text'])
+        self.render('event.html', related_events=related_events, related_papers=related_papers, **event)
 
     def _respond_schedule(self):
         """
         respond with list of event ids preferenced, JSON encoded
         """
-        request = self.api.get_users_preferences(self.user_id)
-        prefs = ujson.loads(request.text)
+        prefs = self.api.get_users_preferences(self.user_id)
         self.set_header(CONTENT_TYPE, APP_JSON)
         self.write(ujson.dumps(prefs))
 
@@ -109,15 +133,16 @@ class ScheduleHandler(APIHandler):
         self.api = api
 
     def get(self, user_id):
-        cards = self.get_schedule_cards(user_id)
-        self.render('schedule.html', user_id=user_id, cards=cards)
+        schedule_cards = self.get_schedule_cards(user_id)
+        self.render('printable_schedule.html', user_id=user_id,
+                    schedule_cards=schedule_cards)
 
 
 def build_application(key):
     resources = {'api': Api(key)}
     application = tornado.web.Application([
-        (r"/{0,1}", RootHandler, resources),
-        (r"/events/([0-9]+)/{0,1}", EventHandler, resources),
+        (r"/{0,1}", EventsHandler, resources),
+        (r"/([0-9]+)/{0,1}", EventHandler, resources),
         (r"/schedule/([A-Za-z-_0-9]+)/{0,1}", ScheduleHandler, resources),
         (r"/(.*\.css)", tornado.web.StaticFileHandler, {'path': './'}), # FIXME path
         (r"/(.*\.js)", tornado.web.StaticFileHandler, {'path': './'}), # FIXME path
