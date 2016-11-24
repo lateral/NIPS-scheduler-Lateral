872d42c8ca5297d2f6376eda2c6427b7626f9aec
Felix <Felix@Felixs-iMac.fritz.box>
2016-11-24 16:07:56 +0100

    Finished single talk page

--- static/main.js
+++ static/main.js
@@ -1,10 +1,15 @@
 $().ready(function() {
+
+	// Add to Schedule
+
 	$(document).on('click', '.add-to-schedule', function(event) {
 		var event_id = $(event.target).data('id');
 		$('#schedule').load('/add', {event_id: event_id});
 		$(event.target).fadeOut("fast")
 		$(event.target).siblings('.remove-from-schedule').fadeIn("fast")
 	});
+
+	// Remove from Schedule
 
 	$(document).on('click', '.remove-from-schedule', function(event) {
 		var event_id = $(event.target).data('id');
@@ -13,12 +18,25 @@ $().ready(function() {
 		$(event.target).siblings('.add-to-schedule').fadeIn("fast")
 	});
 
+	// Search
+
 	$('#search-box').keypress(function(e) {
 	    if(e.which == 13) {
 		var keywords = $(this).val();
 		window.location = '/search?keywords=' + keywords;
 	    }
 	});
+
+	// Related content switcher
+
+	$('.event-single-related h4 span').click(function(e) {
+  	var target = $(this).data('target');
+  	$('.related-source').fadeOut()
+   	$('#' + target).delay(200).fadeIn()
+   	$('.event-single-related h4 span').removeClass("show")
+   	$(this).addClass("show")
+
+  });
 });
 
 
