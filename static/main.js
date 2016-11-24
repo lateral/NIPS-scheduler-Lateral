d716cb6182d40e452515837a30db2b8162350298
Felix <Felix@Felixs-iMac.fritz.box>
2016-11-24 11:35:01 +0100

    Made add / remove event buttons work

--- static/main.js
+++ static/main.js
@@ -2,12 +2,17 @@ $().ready(function() {
 	$(document).on('click', '.add-to-schedule', function(event) {
 		var event_id = $(event.target).data('id');
 		$('#schedule').load('/add', {event_id: event_id});
-		$('.add-to-schedule ').fadeOut()
+		$(event.target).fadeOut("fast")
+		$(event.target).siblings('.remove-from-schedule').fadeIn("fast")
 	});
+
 	$(document).on('click', '.remove-from-schedule', function(event) {
 		var event_id = $(event.target).data('id');
 		$('#schedule').load('/remove', {event_id: event_id});
+		$(event.target).fadeOut("fast")
+		$(event.target).siblings('.add-to-schedule').fadeIn("fast")
 	});
+
 	$('#search-box').keypress(function(e) {
 	    if(e.which == 13) {
 		var keywords = $(this).val();
