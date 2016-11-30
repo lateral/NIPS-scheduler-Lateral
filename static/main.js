a418d1900612fbe820ec654436bc66c97e90397c
Max Novakovic <max@lateral.io>
2016-11-30 17:26:07 +0100

    Add filters

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-$().ready(function() {
+$(function() {
 
 	// Add to Schedule
 
@@ -52,7 +52,7 @@ $().ready(function() {
     }
   });
 
-  // Mobile Switcher 
+  // Mobile Switcher
 
   $('.switcher .left').click(function() {
 	  $('#right-panel').fadeOut()
@@ -69,6 +69,62 @@ $().ready(function() {
 	  $('#right-panel').fadeIn()
 	  $('.switcher .right p').removeClass("show")
 	});
+
+  // Date filter
+  function getHash() {
+    var hash = window.location.hash.replace(/^#!/, '').split('&');
+    console.log(hash)
+    if (hash.length == 1) {
+      window.location.hash = '#!date=all&type=all';
+      hash = window.location.hash.replace(/^#!/, '').split('&');
+    }
+    return [hash[0].replace('date=', ''), hash[1].replace('type=', '')];
+  }
+
+  function updateFiltersFromHash() {
+    var hash = getHash();
+    var dateVal = hash[0];
+    var typeVal = hash[1];
+    if(dateVal == 'all' && typeVal == 'all') {
+      $('#events .event-result').show();
+    } else {
+      var dateString = dateVal.replace('_', ' ');
+      $('#events .event-result').each(function(val){
+        var eventDay = $(this).find('.day').text();
+        if (typeVal == 'all') {
+          $(this).toggle(eventDay == dateString);
+        } else if (dateVal == 'all') {
+          $(this).toggle($(this).find('.event_type').hasClass(typeVal));
+        } else {
+          $(this).toggle(eventDay == dateString && $(this).find('.event_type').hasClass(typeVal));
+        }
+      });
+    }
+    $('.no-events').toggle(!$('#events .event-result:visible').length);
+  }
+
+  $(window).bind('hashchange', updateFiltersFromHash);
+  updateFiltersFromHash();
+  var hash = getHash();
+
+  $('#filter-date select').val(hash[0]);
+  $('#filter-date select').change(function(event) {
+    var val = $(this).val();
+    window.location.hash = '#!date=' + val + '&type=' + getHash()[1];
+  });
+
+  $('#filter-type select').val(hash[1]);
+  $('#filter-type select').change(function(event) {
+    var val = $(this).val();
+    window.location.hash = '#!date=' + getHash()[0] + '&type=' + val;
+  });
+
+  $('.reset-filters').on('click', function(){
+    $('#filter-date select').val('all');
+    $('#filter-type select').val('all');
+    window.location.hash = '#!date=all&type=all';
+  });
+
 });
 
 
